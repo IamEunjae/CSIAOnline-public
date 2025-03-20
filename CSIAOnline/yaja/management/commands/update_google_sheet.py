eaec0ac25bee5b7e7fd8fc8608990fc30a2a650c
ethankim612 <ethansw612@gmail.com>
2025-03-21 00:22:36 +0900

    update_api_links

--- CSIAOnline/yaja/management/commands/update_google_sheet.py
+++ CSIAOnline/yaja/management/commands/update_google_sheet.py
@@ -3,7 +3,7 @@ import datetime
 from ...models import Monday, Tuesday, Wednesday, Thursday  # Update with your app name
 from django.core.management.base import BaseCommand
 
-GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxara4OXHc7uibNKbiUZCkGZVPy4ZuSRhzasATqZZr0H4CdVwlBmiCqZLI-VmV37ma3/exec"
+GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzgcGleKi_yqXuzFwFXPJTMRf1Dfe0F-hcDUrirmgdQuIYITYTq297KsRmB1PxzBuXj/exec"
 
 
 def fetch_schedule():
